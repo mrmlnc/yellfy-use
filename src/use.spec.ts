145dec80ffbcbb8c2e8d13796b09eae4cb98d12d
Denis Malinochkin <dmalinochkin@rambler.ru>
2016-12-10 18:53:09 +0300

    Use tabs

--- src/use.spec.ts
+++ src/use.spec.ts
@@ -6,72 +6,72 @@ import { Use } from './use';
 
 describe('Dependencies', () => {
 
-  it('Should work with default settings.', () => {
-    const $ = new Use({ gulp: null }).use('mocha');
+	it('Should work with default settings.', () => {
+		const $ = new Use({ gulp: null }).use('mocha');
 
-    assert.ok($.mocha);
-  });
+		assert.ok($.mocha);
+	});
 
-  it('Should work with all the dependencies.', () => {
-    const $ = new Use({ gulp: null, dependencies: true }).use('tslint');
+	it('Should work with all the dependencies.', () => {
+		const $ = new Use({ gulp: null, dependencies: true }).use('tslint');
 
-    assert.ok($.tslint);
-  });
+		assert.ok($.tslint);
+	});
 
-  it('Should work with multiple dependencies.', () => {
-    const $ = new Use({ gulp: null, dependencies: true }).use('mocha', 'tslint');
+	it('Should work with multiple dependencies.', () => {
+		const $ = new Use({ gulp: null, dependencies: true }).use('mocha', 'tslint');
 
-    assert.ok($.mocha && $.tslint);
-  });
+		assert.ok($.mocha && $.tslint);
+	});
 
-  it('Should remove the prefix name of the plugin.', () => {
-    const $ = new Use({ gulp: null }).use('gulp-tap');
+	it('Should remove the prefix name of the plugin.', () => {
+		const $ = new Use({ gulp: null }).use('gulp-tap');
 
-    assert.ok($.tap);
-  });
+		assert.ok($.tap);
+	});
 
-  it('Should rename the dependency, as the user wants.', () => {
-    const $ = new Use({ gulp: null }).use('gulp-tap as custom-Tap');
+	it('Should rename the dependency, as the user wants.', () => {
+		const $ = new Use({ gulp: null }).use('gulp-tap as custom-Tap');
 
-    assert.ok($['custom-Tap']);
-  });
+		assert.ok($['custom-Tap']);
+	});
 
-  it('Should work with a custom reporter.', () => {
-    const loader = new Use({
-      gulp: null,
-      reporter: (toInstall) => {
-        assert.equal(toInstall.length, 2);
-        assert.ok(toInstall.indexOf('pikachu') !== -1);
-      }
-    });
+	it('Should work with a custom reporter.', () => {
+		const loader = new Use({
+			gulp: null,
+			reporter: (toInstall) => {
+				assert.equal(toInstall.length, 2);
+				assert.ok(toInstall.indexOf('pikachu') !== -1);
+			}
+		});
 
-    loader.use('yellfy', 'pikachu as error');
-  });
+		loader.use('yellfy', 'pikachu as error');
+	});
 
 });
 
 describe('Helpers', () => {
 
-  it('Should fail if directory of the helpers does not exist.', () => {
-    try {
-      new Use({ gulp: null, helperDir: './notExists' }).use('gulp-tap');
-    } catch (err) {
-      assert.ok(err.message.indexOf('Helpers are not loaded.') !== -1);
-    }
-  });
+	it('Should fail if directory of the helpers does not exist.', () => {
+		try {
+			new Use({ gulp: null, helperDir: './notExists' }).use('gulp-tap');
+		} catch (err) {
+			assert.ok(err.message.indexOf('Helpers are not loaded.') !== -1);
+		}
+	});
 
-  it('Should successfully load helpers.', () => {
-    const $ = new Use({ gulp: null, helperDir: './test/success' }).use('gulp-tap');
+	it('Should successfully load helpers.', () => {
+		const $ = new Use({ gulp: null, helperDir: './test/success' }).use('gulp-tap');
 
-    assert.ok($.helpers.logger);
-  });
+		assert.ok($.helpers.logger);
+	});
 
-  it('Should fail when loading the helper that contains the error.', () => {
-    try {
-      new Use({ gulp: null, helperDir: './test/fail' }).use('gulp-tap');
-    } catch (err) {
-      assert.ok(err.message.indexOf('An error occurred while loading the package') !== -1);
-    }
-  });
+	it('Should fail when loading the helper that contains the error.', () => {
+		try {
+			new Use({ gulp: null, helperDir: './test/fail' }).use('gulp-tap');
+		} catch (err) {
+			assert.ok(err.message.indexOf('An error occurred while loading the package') !== -1);
+		}
+	});
 
 });
--- src/use.ts
+++ src/use.ts
@@ -5,141 +5,141 @@ import * as fs from 'fs';
 import * as camelcase from 'camelcase';
 
 export interface IUseOptions {
-  gulp: any;
-  dependencies?: boolean;
-  devDependencies?: boolean;
-  helperDir?: string;
-  reporter?: (toInstall: string[]) => void;
-  packageFile?: string;
+	gulp: any;
+	dependencies?: boolean;
+	devDependencies?: boolean;
+	helperDir?: string;
+	reporter?: (toInstall: string[]) => void;
+	packageFile?: string;
 }
 
 export interface IUse {
-  options?: IUseOptions;
+	options?: IUseOptions;
 }
 
 export class Use implements IUse {
 
-  private projectDependencies: any;
-  private projectHelpers: any;
+	private projectDependencies: any;
+	private projectHelpers: any;
 
-  constructor(public options: IUseOptions) {
-    const object = <any>Object;
-    this.options = object.assign({
-      reporter: this.reporter,
-      dependencies: false,
-      devDependencies: true,
-      packageFile: './package.json'
-    }, options);
+	constructor(public options: IUseOptions) {
+		const object = <any>Object;
+		this.options = object.assign({
+			reporter: this.reporter,
+			dependencies: false,
+			devDependencies: true,
+			packageFile: './package.json'
+		}, options);
 
-    this.projectDependencies = this.getProjectDependencies();
-    this.projectHelpers = this.getProjectHelpers();
+		this.projectDependencies = this.getProjectDependencies();
+		this.projectHelpers = this.getProjectHelpers();
 
-    this.use = this.use.bind(this);
-  }
+		this.use = this.use.bind(this);
+	}
 
-  public use(...modules: string[]): any {
-    const toInstall: string[] = [];
+	public use(...modules: string[]): any {
+		const toInstall: string[] = [];
 
-    for (let index = 0; index < modules.length; index++) {
-      const dependency = modules[index].replace(/\sas\s.*/, '');
-      if (this.projectDependencies.indexOf(dependency) === -1) {
-        toInstall.push(dependency);
-      }
-    }
+		for (let index = 0; index < modules.length; index++) {
+			const dependency = modules[index].replace(/\sas\s.*/, '');
+			if (this.projectDependencies.indexOf(dependency) === -1) {
+				toInstall.push(dependency);
+			}
+		}
 
-    if (toInstall.length) {
-      this.options.reporter(toInstall);
-    }
+		if (toInstall.length) {
+			this.options.reporter(toInstall);
+		}
 
-    const dependencies: any = {
-      gulp: this.options.gulp,
-      helpers: this.projectHelpers
-    };
+		const dependencies: any = {
+			gulp: this.options.gulp,
+			helpers: this.projectHelpers
+		};
 
-    modules.forEach((dependency) => {
-      const name = this.renameModuleName(dependency);
-      const toRequire = dependency.replace(/\sas\s.*/, '');
-      try {
-        dependencies[name] = require(toRequire);
-      } catch (err) {
-        // silence
-      }
-    });
+		modules.forEach((dependency) => {
+			const name = this.renameModuleName(dependency);
+			const toRequire = dependency.replace(/\sas\s.*/, '');
+			try {
+				dependencies[name] = require(toRequire);
+			} catch (err) {
+				// silence
+			}
+		});
 
-    return dependencies;
-  }
+		return dependencies;
+	}
 
-  private getProjectDependencies(): string[] {
-    let fileData: string;
-    try {
-      fileData = fs.readFileSync(this.options.packageFile, 'utf8');
-    } catch (err) {
-      throw new Error(`Unable to read 'package.json' file. Error: ${err.message}`);
-    }
+	private getProjectDependencies(): string[] {
+		let fileData: string;
+		try {
+			fileData = fs.readFileSync(this.options.packageFile, 'utf8');
+		} catch (err) {
+			throw new Error(`Unable to read 'package.json' file. Error: ${err.message}`);
+		}
 
-    let packageJsonData: any = {};
-    try {
-      packageJsonData = JSON.parse(fileData);
-    } catch (err) {
-      throw new Error(`JSON Parsing Error: ${err.message}`);
-    }
+		let packageJsonData: any = {};
+		try {
+			packageJsonData = JSON.parse(fileData);
+		} catch (err) {
+			throw new Error(`JSON Parsing Error: ${err.message}`);
+		}
 
-    let packageDependencies: string[] = [];
-    if (this.options.dependencies) {
-      packageDependencies = packageDependencies.concat(Object.keys(packageJsonData.dependencies));
-    }
+		let packageDependencies: string[] = [];
+		if (this.options.dependencies) {
+			packageDependencies = packageDependencies.concat(Object.keys(packageJsonData.dependencies));
+		}
 
-    if (this.options.devDependencies) {
-      packageDependencies = packageDependencies.concat(Object.keys(packageJsonData.devDependencies));
-    }
+		if (this.options.devDependencies) {
+			packageDependencies = packageDependencies.concat(Object.keys(packageJsonData.devDependencies));
+		}
 
-    return packageDependencies;
-  }
+		return packageDependencies;
+	}
 
-  private reporter(toInstall: string[]): void {
-    console.error(`Something is not enough. Use 'npm i -D %s'`, toInstall.join(' '));
-  }
+	private reporter(toInstall: string[]): void {
+		console.error(`Something is not enough. Use 'npm i -D %s'`, toInstall.join(' '));
+	}
 
-  private renameModuleName(name: string): string {
-    const newName = /\sas\s(.*)/.exec(name);
-    if (newName) {
-      return newName[1];
-    }
+	private renameModuleName(name: string): string {
+		const newName = /\sas\s(.*)/.exec(name);
+		if (newName) {
+			return newName[1];
+		}
 
-    return camelcase(name.replace(/^gulp(-|\.)/, ''));
-  }
+		return camelcase(name.replace(/^gulp(-|\.)/, ''));
+	}
 
-  private getProjectHelpers(): any {
-    if (!this.options.helperDir) {
-      return {};
-    }
+	private getProjectHelpers(): any {
+		if (!this.options.helperDir) {
+			return {};
+		}
 
-    let files: string[];
-    try {
-      files = fs.readdirSync(this.options.helperDir);
-    } catch (err) {
-      throw new Error(`Helpers are not loaded. Error: ${err.message}`);
-    }
+		let files: string[];
+		try {
+			files = fs.readdirSync(this.options.helperDir);
+		} catch (err) {
+			throw new Error(`Helpers are not loaded. Error: ${err.message}`);
+		}
 
-    const projectHelpers: any = {};
-    for (let index = 0; index < files.length; index++) {
-      const isFile = path.extname(files[index]) === '.js';
-      if (!isFile) {
-        continue;
-      }
+		const projectHelpers: any = {};
+		for (let index = 0; index < files.length; index++) {
+			const isFile = path.extname(files[index]) === '.js';
+			if (!isFile) {
+				continue;
+			}
 
-      const basename = path.basename(files[index], '.js');
-      const helperName = this.renameModuleName(basename);
-      const helperPath = path.join(process.cwd(), this.options.helperDir, basename);
+			const basename = path.basename(files[index], '.js');
+			const helperName = this.renameModuleName(basename);
+			const helperPath = path.join(process.cwd(), this.options.helperDir, basename);
 
-      try {
-        projectHelpers[helperName] = require(helperPath);
-      } catch (err) {
-        throw new Error(`An error occurred while loading the package: ${err.message}`);
-      }
-    }
+			try {
+				projectHelpers[helperName] = require(helperPath);
+			} catch (err) {
+				throw new Error(`An error occurred while loading the package: ${err.message}`);
+			}
+		}
 
-    return projectHelpers;
-  }
+		return projectHelpers;
+	}
 
 }
